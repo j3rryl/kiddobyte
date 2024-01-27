624582120f0b52d3c918d86d896c3b8804cdf3d9
j3rryl <jeremymunroe0@gmail.com>
2024-01-27 06:55:26 +0300

    login controller

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 
 dependencies {
 
+    //Lottie Animations
+    implementation("com.airbnb.android:lottie:3.7.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
