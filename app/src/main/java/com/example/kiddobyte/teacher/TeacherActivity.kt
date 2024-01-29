fda6de5adc28bc3ee76baa9208bafb9a5c08b4ed
j3rryl <jeremymunroe0@gmail.com>
2024-01-29 20:33:33 +0300

    hello there

---
+++ app/src/main/java/com/example/kiddobyte/teacher/TeacherActivity.kt
@@ -0,0 +1,48 @@
+package com.example.kiddobyte.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.kiddobyte.R
+import com.example.kiddobyte.databinding.ActivityTeacherBinding
+import com.example.kiddobyte.teacher.fragments.ModulesFragment
+import com.example.kiddobyte.teacher.fragments.StatisticsFragment
+import com.example.kiddobyte.teacher.fragments.SupportFragment
+
+class TeacherActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTeacherBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTeacherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadFragment(StatisticsFragment())
+        binding.bottomNavigationBar.setItemSelected(R.id.nav_home, true)
+        binding.bottomNavigationBar.showBadge(R.id.nav_recipes, 15)
+
+        setUpTabBar()
+
+    }
+    private fun setUpTabBar(){
+        binding.bottomNavigationBar.setOnItemSelectedListener {
+            var fragment: Fragment = StatisticsFragment()
+            when(it){
+                R.id.nav_home-> {
+                    fragment= StatisticsFragment()
+                }
+                R.id.nav_recipes-> {
+                    fragment= ModulesFragment()
+                }
+                R.id.nav_contact-> {
+                    fragment= SupportFragment()
+                }
+            }
+            loadFragment(fragment)
+        }
+    }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
+    }
+}
