99723d6ce2ac81e1bda9dae8d73c83e43d483c95
j3rryl <jeremymunroe0@gmail.com>
2024-01-29 20:30:34 +0300

    firebase

--- app/src/main/java/com/example/kiddobyte/authentication/LoginActivity.kt
+++ app/src/main/java/com/example/kiddobyte/authentication/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.kiddobyte.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kiddobyte.R
+import com.example.kiddobyte.databinding.ActivityLoginBinding
+import com.example.kiddobyte.teacher.TeacherActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, TeacherActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
